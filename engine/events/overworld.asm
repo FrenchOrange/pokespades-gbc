8259eefe6a501848748fe03fc28679e8c2cf0bd8
FrenchOrange <64930003+FrenchOrange@users.noreply.github.com>
2023-10-22 17:45:26 +0200

    Forest permission fix
    * Fixed issue where the player couldn't get on the bike in Serenity Forest.
    * Fixed issue where the player couldn't use Dig/Escape Ropes in Serenity Forest.

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -775,6 +775,8 @@ EscapeRopeOrDig:
 	jr z, .incave
 	cp DUNGEON
 	jr z, .incave
+	cp FOREST
+	jr z, .incave
 .fail
 	ld a, $2
 	ret
@@ -1691,6 +1693,8 @@ BikeFunction:
 	cp CAVE
 	jr z, .ok
 	cp GATE
+	jr z, .ok
+	cp FOREST
 	jr z, .ok
 	jr .nope
 
